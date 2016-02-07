4b4298340e0bfd981de7a60c8efeeca0fd052040
Andrew Berezovskiy <andrew@berezovskiy.me>
2016-02-07 15:18:02 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+chrome-unlink
+==============================================================================
+
+This Chrome extensions allows you to skip link advertising services by using
+the extension via a right-click on the shortened link.
---
+++ main.js
@@ -0,0 +1,25 @@
+skipLinkShortener = function(arg){
+  var srcUrl = arg.linkUrl;
+  var dstUrl = srcUrl; // safe default
+
+  console.log("Source " + srcUrl);
+
+  var shst_regex = /http:\/\/sh\.st\/st\/(\w+)\/http:\/\/(.*)$/
+  var match = srcUrl.match(shst_regex);
+  if(match != null && match.length >= 3) {
+    console.log("Match " + match);
+
+    dstUrl = "http://" + match[2];
+  }
+  // TODO alternate between approches based on 'Ctrl' key status
+  // chrome.tabs.create({url: dstUrl});
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    chrome.tabs.update(tabs[0].id, {url: dstUrl});
+  });
+};
+
+chrome.contextMenus.create({
+  title: "Skip the link shortener",
+  contexts:["link"],
+  onclick: skipLinkShortener
+});
